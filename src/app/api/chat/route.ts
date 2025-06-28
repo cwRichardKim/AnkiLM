90307713389545a9397299041275e86ee199e3b9
richard_kim <richard.kim@airbnb.com>
2025-06-28 12:40:21 -0400

    basics of calling a backend

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,10 +1,18 @@
+import { MessageType } from "@/app/components/Message";
 import { NextRequest, NextResponse } from "next/server";
+
+export interface ChatRequest {
+  messages: MessageType[];
+  command: "explain" | "review";
+}
+
 export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
-    const body = await req.json();
-    const { message, cardContext, command } = body;
+    const body = (await req.json()) as ChatRequest;
+    console.log("Received body", body);
+    const { messages, command } = body;
     const mockResponse = {
-      response: `Mock response to "${message} ${cardContext}"`,
+      response: `Mock response to "${messages}"`,
       command: command || "explain",
     };
     return NextResponse.json(mockResponse);
--- src/app/components/ChatContainer.tsx
+++ src/app/components/ChatContainer.tsx
@@ -17,16 +17,28 @@ export default function ChatContainer({
   const [isStreaming, setIsStreaming] = useState(false);
   const [streamingMessage] = useState<MessageType | null>(null);
   const sendMessage = useCallback(
-    (message: string) => {
+    async (message: string) => {
       setInput("");
       setIsStreaming(true);
       setMessages((prev) => [...prev, { role: "user", content: message }]);
+      fetch("/api/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          messages: messages,
+          command: "explain",
+        }),
+      }).then((res) => {
+        console.log(res);
+      });
       // send post request to /api/chat
       // listen for streaming response
       // set streaming response to streaming message
       console.log(message);
     },
-    [setInput]
+    [messages]
   );
   console.log(card, reviewCard);
   return (
